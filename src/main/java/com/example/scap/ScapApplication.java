6f088333df1acb1ca3e7aa655d08b049a7d3fd9d
mladBits <167035155+mladBits@users.noreply.github.com>
2026-04-15 14:04:37 -0600

    initial commit

---
+++ src/main/java/com/example/scap/ScapApplication.java
@@ -0,0 +1,13 @@
+package com.example.scap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scap/ScapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
